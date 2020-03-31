89d84ddcde4bbbe93a05713375167b148b7b7e0d
Yen Truong <truthblue82@gmail.com>
2020-03-31 11:34:02 +0700

    add code

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './FullPost.css';
+
+class FullPost extends Component {
+    state = {
+        loadedPosts: null
+    }
+
+    componentDidUpdate () {
+        if (this.props.id) {
+            if (!this.state.loadedPosts || (this.state.loadedPosts && this.state.loadedPosts.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({loadedPosts: response.data});
+                    });
+            }
+        }
+    }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                //
+            });
+    }
+
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+        if (this.state.loadedPosts)
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPosts.title}</h1>
+                    <p>{this.state.loadedPosts.content}</p>
+                    <div className="Edit">
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+                    </div>
+                </div>
+
+            );
+        return post;
+    }
+}
+
+export default FullPost;
